db07b755c486970f9fbda8a202e931303a58dc99
forhad22 <69205034+forhad22@users.noreply.github.com>
2021-05-23 18:08:57 +0600

    How To Use Tik_Tik.
    type: tty-clock

---
+++ tik_tik.py
@@ -0,0 +1,13 @@
+#GNU nano 5.7   tik_tik.py
+#/usr/bin/python2
+
+import os
+
+#This tool is coded in python2 by Forhad
+
+os.system("clear")
+os.system("apt update && apt upgrade")
+os.system("pkg install tty-clock")
+os.system("cd $HOME")
+os.system("figlet Forhd")
+os.system("tty-clock -t")
